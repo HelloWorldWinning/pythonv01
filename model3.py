7692dafe0a10a921c89117be9821466a54ba508b
z <112498488@qq.com>
2018-06-22 15:09:18 +0700

    vnew03

--- model3.py
+++ model3.py
@@ -1,9 +1,7 @@
 import numpy as np
 import subprocess
 import os
-
 # np.set_printoptions(precision=20)
-
 
 class MODEL_JPG_VECTOR(object):
 
@@ -35,40 +33,29 @@ class MODEL_JPG_VECTOR(object):
         else:
             raise Exception("at least specify img_path_many or folder_path")
 
-        self.Get_Target()
+        self._Get_Target()
 
     def _Img_List(self):
         folder_path = self.folder_path
         movies_jpgs = subprocess.check_output(["ls", folder_path]).decode("utf-8").split("\n")
         movies_jpgs = [os.path.join(folder_path, i) for i in movies_jpgs if i.endswith(".jpg")]
         # print("movies_jpgs = " ,movies_jpgs)
-        img_path_many = movies_jpgs[10003:10000+5]
+        img_path_many = movies_jpgs[10003:10000+5+5+3]
         # print(img_path_many)
+        if isinstance(img_path_many,str):
+            img_path_many = [img_path_many]
         return img_path_many
 
     def Jpg_To_Vector(self, img_path):
-
         self.a1_img_path = img_path
         img = self._image.load_img(img_path, target_size=(299, 299))
-
         x = self._image.img_to_array(img)
-        # self.a2_x_array_copy = x.copy()
-        # self.a2_foo = x
-        # x = x.astype("float32")
-
-        self.a2_x_array = x
-
         x = np.expand_dims(x, axis=0)
-        # self.a3_x_expand_copy = x.copy()
-        # x = x.astype("float32")
-        self.a3_x_expand  = x
-
         x = self._preprocess_input(x)
-        # self.a4_preprocessed_copy = x.copy()
-        self.a4_preprocessed = x
+        # self.a4_preprocessed = x
 
         output_of_model = self._model.predict(x.copy(), batch_size=1)
-        self.a5_output = output_of_model
+        # self.a5_output = output_of_model
         self.a6_data = output_of_model.reshape(1, -1)
 
         return self.a6_data
@@ -81,7 +68,7 @@ class MODEL_JPG_VECTOR(object):
         if img_path_many ==None:
             img_path_many = list(self.img_path_many)
         elif isinstance(img_path_many,str):
-            print("str or nor ".center(50,"%"))
+            # print("str or nor ".center(50,"%"))
             img_path_many = [img_path_many]
         elif isinstance(img_path_many,list):
             pass
@@ -108,54 +95,35 @@ class MODEL_JPG_VECTOR(object):
             temp_zero_array = np.zeros([temp_chunk, 299, 299, 3], dtype=np.float32)
 
             for index, img_path in enumerate(img_path_many[start_of_slice:end_of_slice]):
-
-                self.b1_img_path = img_path
+                # self.b1_img_path = img_path
                 img = self._image.load_img(img_path, target_size=(299, 299))
-
-                self.b_image = img
+                # self.b_image = img
                 x = self._image.img_to_array(img)
-
-                self.b2_x_array = x
+                # self.b2_x_array = x
                 x = np.expand_dims(x, axis=0)
-
                 temp_zero_array[index] = x
-
-                print("name_index =", name_index, "temp_chunk =", temp_chunk, "temp_zero_array.shape =", temp_zero_array.shape)
+                # print("name_index =", name_index, "temp_chunk =", temp_chunk, "temp_zero_array.shape =", temp_zero_array.shape)
                 name_index += 1
 
             temp_zero_array /= 127.5
             temp_zero_array -= 1.
 
-            self.b3_x_expand_temp_zero_array = temp_zero_array
-
-            # x = self._preprocess_input(temp_zero_array)
-            # self.b4_preprocessed = x
-            # output_of_model = self._model.predict(x, batch_size=32 * 5)
-
+            # self.b3_x_expand_temp_zero_array = temp_zero_array
             output_of_model = self._model.predict(temp_zero_array, batch_size=1)
-
-            self.b5_output = output_of_model
+            # self.b5_output = output_of_model
             data = output_of_model.reshape(temp_chunk, -1)
-            self.b6_data = data
-            # print(data)
+            # self.b6_data = data
             # print("data.shape =",data.shape, self.target[start_of_slice:end_of_slice].shape)
-
             i += 1
 
         if to_database:
             print("to database")
-            # if one_flag:
-            #     print("one_flag")
-            #     return  data[-1]
             return data
         else:
             print("to query")
-            # if one_flag:
-            # #     print("one_flag")
-            #     return  data[-1]
             return data
 
-    def Get_Target(self):
+    def _Get_Target(self):
         img_path_many = self.img_path_many
         name_fps = np.zeros([len(img_path_many), 2])
         frames_location = np.zeros(len(img_path_many))
@@ -171,11 +139,9 @@ class MODEL_JPG_VECTOR(object):
 folder_path = "/data/bar03/output"
 
 bar = MODEL_JPG_VECTOR(folder_path=folder_path)
-# ten_jpg = bar.Jpg_To_Vector_DataBase(to_database=False)
+
+ten_jpg = bar.Jpg_To_Vector_DataBase(to_database=False)
 one_jpg = bar.Jpg_To_Vector_DataBase(img_path_many=bar.img_path_many[-1:],to_database=True)
-one_jpg_1 = bar.Jpg_To_Vector_DataBase(img_path_many=bar.img_path_many[-1],to_database=True)
+# one_jpg_1 = bar.Jpg_To_Vector_DataBase(img_path_many=bar.img_path_many[-1],to_database=True)
 print(  "print( ( one_jpg.flatten() == one_jpg_1.flatten() ).all() )".center(100)  )
-print( ( one_jpg.flatten() == one_jpg_1.flatten() ).all() )
-# ten_jpg
-# print((bar.b3_x_expand == bar.a3_x_expand).all(), "(bar.b3_x_expand == bar.a3_x_expand).all()")
-# print(  "(one_jpg.flatten() == ten_jpg[-1].flatten()).all() =" ,(one_jpg.flatten() == ten_jpg[-1].flatten()).all()  )
+print( ( one_jpg.flatten() ==ten_jpg[-1].flatten() ).all() )
